d84fee83e14fe04d26982b22be855ecfb239d3af
zenziski <matheus.zenziski@gmail.com>
2023-07-27 03:12:41 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ const nextConfig = {
   //Por algum motivo isso aqui não está pegando o ENV da maquina, enquanto não vemos isso, 
   //Essa informação ficará estática aqui 
   env: {
-    //NEXT_PUBLIC_BACKEND_API: "http://154.56.40.212:3001",
-    NEXT_PUBLIC_BACKEND_API: "http://localhost:3001"
+    NEXT_PUBLIC_BACKEND_API: "http://154.56.40.212:3001",
+    //NEXT_PUBLIC_BACKEND_API: "http://localhost:3001"
   },
 }
 module.exports = nextConfig
